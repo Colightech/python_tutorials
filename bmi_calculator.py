6a72a8a089118edc9f74c9762697920804e7bf98
Colightech <136013493+Colightech@users.noreply.github.com>
2023-10-10 12:12:06 +0100

    Update bmi_calculator.py
    updated BMI calculator

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+print("THIS PROGRAM COMPUTES THE BODY MASS INDEX (BMI)")
 Height=float(input("Enter your height in centimeters: "))
 Weight=float(input("Enter your weight in kg: "))
 Height = Height/100
@@ -6,12 +7,12 @@ BMI=Weight/(Height*Height)
 print("your Body Mass Index is: ", BMI)
 if BMI > 0:
     if BMI <= 16:
-        print("you are severely underweight.")
+        print("Result >>>are severely underweight<<<")
     elif BMI <= 18.5:
-        print("you are underweight.")
+        print("Result >>>you are underweight<<<")
     elif BMI <= 25:
-        print("you are healthy.")
+        print("Result >>>you are healthy<<<")
     elif BMI <= 30:
-        print("you are overweight.")
-    else: print("you are severely overweight.")
-else: ("Enter valid details.")
+        print("Result >>>you are overweight<<<")
+    else: print("Result >>>you are severely overweight<<<")
+else: (">>>Enter valid details<<<")
